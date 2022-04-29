8b19111d42e86b874421c559f4733185a04e6e88
agakhil13 <aguptaji1310@gmail.com>
2022-04-29 19:17:27 +0530

    Asyncconsumer

--- app/consumers.py
+++ app/consumers.py
@@ -1,6 +1,6 @@
 #Topic -- Consumer
 
-from channels.consumer import SyncConsumer
+from channels.consumer import SyncConsumer, AsyncConsumer
 
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
@@ -10,4 +10,14 @@ class MySyncConsumer(SyncConsumer):
         print('message received...')
     
     def websocket_disconnect(self, event):
+        print('websocket disconnected')
+
+class MyAsyncConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        print('websocket connected')
+    
+    async def websocket_receive(self, event):
+        print('message received...')
+    
+    async def websocket_disconnect(self, event):
         print('websocket disconnected')
