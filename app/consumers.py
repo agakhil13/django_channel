b3f328052e417bc89c7765a8251e0c410923495b
agakhil13 <aguptaji1310@gmail.com>
2022-04-30 15:41:36 +0530

    routing and websocket accept

--- app/consumers.py
+++ app/consumers.py
@@ -5,6 +5,10 @@ from channels.consumer import SyncConsumer, AsyncConsumer
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
         print('websocket connected')
+        self.send({
+            'type': 'websocket.accept'
+        }
+        )
     
     def websocket_receive(self, event):
         print('message received...')
--- gs1/asgi.py
+++ gs1/asgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+import app.routing
+
 
 from django.core.asgi import get_asgi_application
 
@@ -16,4 +18,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gs1.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': URLRouter(
+        app.routing.websocket_urlpatterns
+    )
 })
