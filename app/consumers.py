aa334fbb41a8ed8b7773acd975b7f4fa043e9a78
agakhil13 <aguptaji1310@gmail.com>
2022-04-30 15:51:26 +0530

    routing and websocket accept

--- app/consumers.py
+++ app/consumers.py
@@ -4,24 +4,25 @@ from channels.consumer import SyncConsumer, AsyncConsumer
 
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
-        print('websocket connected')
+        print('websocket connected',event)
         self.send({
             'type': 'websocket.accept'
         }
         )
     
     def websocket_receive(self, event):
-        print('message received...')
+        print('message received...', event)
+        print("Message is: " + event['text'])
     
     def websocket_disconnect(self, event):
-        print('websocket disconnected')
+        print('websocket disconnected', event)
 
 class MyAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
-        print('websocket connected')
+        print('websocket connected', event)
     
     async def websocket_receive(self, event):
-        print('message received...')
+        print('message received...', event)
     
     async def websocket_disconnect(self, event):
-        print('websocket disconnected')
+        print('websocket disconnected', event)
