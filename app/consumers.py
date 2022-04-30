e32bbef631cbda71c05112e17cdffd130dcdb03d
agakhil13 <aguptaji1310@gmail.com>
2022-04-30 17:52:15 +0530

    send and receive message

--- app/consumers.py
+++ app/consumers.py
@@ -12,9 +12,11 @@ class MySyncConsumer(SyncConsumer):
         )
     
     def websocket_receive(self, event):
-        print('message received...', event)
         print("Message is: " + event['text'])
-    
+        self.send({
+            'type': 'websocket.send',
+            'text': 'Thankyou for Message'
+        })
     def websocket_disconnect(self, event):
         print('websocket disconnected', event)
         raise StopConsumer()
@@ -22,6 +24,10 @@ class MySyncConsumer(SyncConsumer):
 class MyAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print('websocket connected', event)
+        await self.send({
+            'type': 'websocket.accept'
+        }
+        )
     
     async def websocket_receive(self, event):
         print('message received...', event)
