96bb8cd971c17272d1df03b869f6d353cd50f1fb
agakhil13 <aguptaji1310@gmail.com>
2022-05-01 08:40:51 +0530

    sync and async diff

--- app/consumers.py
+++ app/consumers.py
@@ -2,21 +2,25 @@
 
 from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
+from time import sleep
+import asyncio
 
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
         print('websocket connected',event)
         self.send({
             'type': 'websocket.accept'
-        }
-        )
+        })
     
     def websocket_receive(self, event):
         print("Message is: " + event['text'])
-        self.send({
+        for i in range(50):
+            self.send({
             'type': 'websocket.send',
-            'text': 'Thankyou for Message'
-        })
+            'text': str(i)
+            })
+            sleep(1)
+
     def websocket_disconnect(self, event):
         print('websocket disconnected', event)
         raise StopConsumer()
@@ -26,11 +30,16 @@ class MyAsyncConsumer(AsyncConsumer):
         print('websocket connected', event)
         await self.send({
             'type': 'websocket.accept'
-        }
-        )
+        })
     
     async def websocket_receive(self, event):
         print('message received...', event)
+        for i in range(50):
+            await self.send({
+            'type': 'websocket.send',
+            'text': str(i)
+            })
+            await asyncio.sleep(1)
     
     async def websocket_disconnect(self, event):
         print('websocket disconnected', event)
